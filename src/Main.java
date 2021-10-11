28ad90238f85dbb09c8e1de6736ca58c24660805
evgeniy150589 <epritula89@mail.ru>
2021-10-11 12:21:20 +0300

    JAVADZ2.2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientAccount = 100;
+        int replenishment = 900;
+        int bonus = replenishment / 100;
+        int total ;
+if (replenishment >= 1000)
+        {
+            total = clientAccount + replenishment + bonus;
+            System.out.println("На счету " + total);
+            System.out.println("Бонус " + bonus);
+        } else {
+    total = clientAccount + replenishment;
+            System.out.println("На счету " +total);
+        }
+    }
+}
